97e8100e37583671c991cde1c0a65c4713952f31
UnderAnder <andryushin.alexander@gmail.com>
2021-04-09 19:22:31 +0700

    Stage 3/6: Where to next?

--- game.py
+++ game.py
@@ -3,19 +3,29 @@ class Board:
         self.col = size[0]
         self.row = size[1]
         self.placeholder = '_' * len(str(self.col * self.row))
-        self.table = {(x, y): self.placeholder for y in range(self.row, 0, -1) for x in range(1, self.col + 1)}
+        self.placeholder_len = len(self.placeholder)
+        self.board = {(x, y): self.placeholder for y in range(self.row, 0, -1) for x in range(1, self.col + 1)}
 
-    def draw_board(self, cell):
-        board = self.table
-        board[cell] = ' ' * len(self.placeholder[:-1]) + 'X'
-        print(' ---', '-' * (len(self.placeholder) + 1) * self.col, sep='')
+    def draw_board(self):
+        print(' ---', '-' * (self.placeholder_len + 1) * self.col, sep='')
         for y in range(self.row, 0, -1):
             print(' ' * (len(str(self.row)) - len(str(y))), y, '| ', sep='', end='')
-            for x in range(1, self.col + 1):
-                print(board[x, y], '', end='')
-            print('|')
-        print(' ---', '-' * (len(self.placeholder) + 1) * self.col, sep='')
-        print('  ', *[' ' * (len(self.placeholder)) + str(i) for i in range(1, self.col + 1)], sep='')
+            print(*[''.join(self.board[x, y]) for x in range(1, self.col + 1)], end=" |\n")
+        print(' ---', '-' * (self.placeholder_len + 1) * self.col, sep='')
+        print('  ', *[' ' * self.placeholder_len + str(i) for i in range(1, self.col + 1)], sep='')
+
+    def place_knight(self, cell):
+        self.board[cell] = ' ' * (self.placeholder_len - 1) + 'X'
+        self.possible_moves(cell)
+
+    def possible_moves(self, cell: tuple):
+        col, row = cell[0], cell[1]
+        for i in (-2, -1, 1, 2):
+            for j in (-2, -1, 1, 2):
+                if abs(i) == abs(j):
+                    continue
+                if (col + j, row + i) in self.board.keys():
+                    self.board[(col + j, row + i)] = ' ' * (self.placeholder_len - 1) + 'O'
 
 
 def dimensions_input():
@@ -45,7 +55,8 @@ def position_input(dimensions):
 def main():
     dimensions = dimensions_input()
     board = Board(dimensions)
-    board.draw_board(position_input(dimensions))
+    board.place_knight(position_input(dimensions))
+    board.draw_board()
 
 
 if __name__ == '__main__':
